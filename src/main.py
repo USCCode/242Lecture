43f46ef1c2b0aa34f97fded1dca56390bbbb8f44
Jose M Vidal <jmvidal@gmail.com>
2012-09-17 09:54:54 -0400

    September 17 lecture end.

--- src/main.py
+++ src/main.py
@@ -12,6 +12,9 @@ from google.appengine.ext import db
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
 
+class Post(db.Model):
+    content = db.StringProperty()
+
 class MyHandler(webapp2.RequestHandler):
 
     def render(self, afile):
@@ -22,7 +25,16 @@ class MyHandler(webapp2.RequestHandler):
     def get(self):
         self.templateValues = {}
         self.templateValues['title'] = 'This is the title.'
+        query = Post.all()
+        self.templateValues['posts'] = query
         self.render('base.html')
+        
+    def post(self):
+        content = self.request.get('theContent')
+        newPost = Post(content=content)
+        newPost.put()
+        self.redirect('/')
+        
     
 
 app = webapp2.WSGIApplication([('/.*', MyHandler)], debug = True)
