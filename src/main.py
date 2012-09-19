66cfc50004d24f7dc9f4829c322b0f73efe34098
Jose M Vidal <jmvidal@gmail.com>
2012-09-19 17:09:42 -0400

    Messing around with decorators.

--- src/main.py
+++ src/main.py
@@ -16,40 +16,74 @@ class Post(db.Model):
     content = db.StringProperty()
     stars = db.IntegerProperty()
 
+def onlyLoggedin(method):
+    def _onlyLoggedin(*args,**kw):
+        logging.info('decorator')
+        self = args[0]
+        self.user = users.get_current_user()
+        if self.user:
+            self.templateValues = {}            
+            self.templateValues['login'] = ('%s <a href="%s">Logout</a> ' 
+                                            % (self.user.nickname(), users.create_logout_url('/')))            
+            method(*args, **kw)
+        else:
+            self.redirect('/')
+    return _onlyLoggedin
+
+
 class MyHandler(webapp2.RequestHandler):
 
     def render(self, afile):
         "Render the given file"
         template = jinja_environment.get_template(afile)
         self.response.out.write(template.render(self.templateValues))
+        
+    def setupUser(self):
+        self.user = users.get_current_user()
+        if self.user:
+            self.templateValues = {}
+            self.templateValues['login'] = ('%s <a href="%s">Logout</a> ' 
+                                            % (self.user.nickname(), users.create_logout_url('/')))
+        else:
+            self.redirect('/')
+            return False
+        return True
     
     def get(self): # /
         self.templateValues = {}
-        self.templateValues['title'] = 'Datastore Tutorial'
-        self.templateValues['posts'] = Post.all().filter('stars >=', 3)
-        key = db.Key.from_path('Post',3) ## Get the key for ID=4
-#        self.templateValues['chosen'] = db.get(key).content #Get Post
-        self.templateValues['chosen'] = self.request.path
+        self.templateValues['title'] = 'Organization'
+        
+        user = users.get_current_user()
+        if user:
+            self.templateValues['login'] = ('%s <a href="%s">Logout</a> ' 
+                                            % (user.nickname(), users.create_logout_url('/')))
+        else:
+            self.templateValues['login'] = ('<a href="%s">Login</a>' % users.create_login_url(self.request.uri))        
         self.render('base.html')
         
-    def post(self): # / 
-        content = self.request.get('theContent')
-        stars = int(self.request.get('stars'))
-        if (content != ''):
-            newPost = Post(content=content,stars=stars)
-            newPost.put()
-        self.redirect('/')
-        
-class SplitHandler(MyHandler):
-    def get(self):
-        path = self.request.path.split('/')
-        self.templateValues = {}
-        self.templateValues['title'] = 'Split Test'
-        self.templateValues['posts'] = path
-        self.render('split.html')
-        
 
-app = webapp2.WSGIApplication([('/split/.*', SplitHandler), 
+class BlogHandler(MyHandler):    
+
+    @onlyLoggedin        
+    def get(self): # /blog
+        logging.info('in get bloghandler')
+        self.templateValues = {}
+        self.templateValues['title'] = 'Blog'
+        logging.info('here we are')
+        self.render('base.html')    
+
+        
+class PostHandler(MyHandler):
+
+    def get(self): # /post
+        self.templateValues = {}
+        self.templateValues['title'] = 'Post'
+        logging.info('here we are')
+        self.render('base.html')            
+            
+
+        
+app = webapp2.WSGIApplication([('/blog.*', BlogHandler),('/post.*', PostHandler),
                                ('/.*', MyHandler)], debug = True)
     
 
