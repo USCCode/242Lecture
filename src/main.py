d7e4f7e8d44abea8ad5a8bf5632be9e7e2dac365
Jose M Vidal <jmvidal@gmail.com>
2012-09-19 10:03:50 -0400

    End of lecture September 19.

--- src/main.py
+++ src/main.py
@@ -14,6 +14,7 @@ jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.di
 
 class Post(db.Model):
     content = db.StringProperty()
+    stars = db.IntegerProperty()
 
 class MyHandler(webapp2.RequestHandler):
 
@@ -25,17 +26,31 @@ class MyHandler(webapp2.RequestHandler):
     def get(self): # /
         self.templateValues = {}
         self.templateValues['title'] = 'Datastore Tutorial'
-        self.templateValues['posts'] = Post.all()
+        self.templateValues['posts'] = Post.all().filter('stars >=', 3)
+        key = db.Key.from_path('Post',3) ## Get the key for ID=4
+#        self.templateValues['chosen'] = db.get(key).content #Get Post
+        self.templateValues['chosen'] = self.request.path
         self.render('base.html')
         
     def post(self): # / 
         content = self.request.get('theContent')
+        stars = int(self.request.get('stars'))
         if (content != ''):
-            newPost = Post(content=content)
+            newPost = Post(content=content,stars=stars)
             newPost.put()
         self.redirect('/')
+        
+class SplitHandler(MyHandler):
+    def get(self):
+        path = self.request.path.split('/')
+        self.templateValues = {}
+        self.templateValues['title'] = 'Split Test'
+        self.templateValues['posts'] = path
+        self.render('split.html')
+        
 
-app = webapp2.WSGIApplication([('/.*', MyHandler)], debug = True)
+app = webapp2.WSGIApplication([('/split/.*', SplitHandler), 
+                               ('/.*', MyHandler)], debug = True)
     
 
 
