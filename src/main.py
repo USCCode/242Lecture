c10b8b619dd5dc98904e0a7332dda1276c89ae64
Jose M Vidal <jmvidal@gmail.com>
2012-09-19 19:51:35 -0400

    end of Screencast on Event Handlers

--- src/main.py
+++ src/main.py
@@ -16,75 +16,41 @@ class Post(db.Model):
     content = db.StringProperty()
     stars = db.IntegerProperty()
 
-def onlyLoggedin(method):
-    def _onlyLoggedin(*args,**kw):
-        logging.info('decorator')
-        self = args[0]
-        self.user = users.get_current_user()
-        if self.user:
-            self.templateValues = {}            
-            self.templateValues['login'] = ('%s <a href="%s">Logout</a> ' 
-                                            % (self.user.nickname(), users.create_logout_url('/')))            
-            method(*args, **kw)
-        else:
-            self.redirect('/')
-    return _onlyLoggedin
-
-
 class MyHandler(webapp2.RequestHandler):
-
-    def render(self, afile):
-        "Render the given file"
-        template = jinja_environment.get_template(afile)
-        self.response.out.write(template.render(self.templateValues))
-        
+    
     def setupUser(self):
         self.user = users.get_current_user()
         if self.user:
-            self.templateValues = {}
             self.templateValues['login'] = ('%s <a href="%s">Logout</a> ' 
                                             % (self.user.nickname(), users.create_logout_url('/')))
         else:
-            self.redirect('/')
-            return False
-        return True
-    
+            self.templateValues['login'] = ('<a href="%s">Login</a>' % 
+                                            users.create_login_url(self.request.uri))
+            
+    def render(self,afile):
+        template = jinja_environment.get_template(afile)
+        self.response.out.write(template.render(self.templateValues))
+        
+class MainpageHandler(MyHandler):
     def get(self): # /
         self.templateValues = {}
-        self.templateValues['title'] = 'Organization'
+        self.templateValues['title'] = 'Main Page'
+        self.setupUser()
+        self.render("base.html")
         
-        user = users.get_current_user()
-        if user:
-            self.templateValues['login'] = ('%s <a href="%s">Logout</a> ' 
-                                            % (user.nickname(), users.create_logout_url('/')))
-        else:
-            self.templateValues['login'] = ('<a href="%s">Login</a>' % users.create_login_url(self.request.uri))        
+class BlogHandler(MyHandler):
+    def get(self):
+        self.templateValues = {}
+        self.templateValues['title'] = 'Blog Page'
+        self.setupUser()
+        if not self.user:
+            self.redirect('/')
+            return
         self.render('base.html')
-        
-
-class BlogHandler(MyHandler):    
-
-    @onlyLoggedin        
-    def get(self): # /blog
-        logging.info('in get bloghandler')
-        self.templateValues = {}
-        self.templateValues['title'] = 'Blog'
-        logging.info('here we are')
-        self.render('base.html')    
 
         
-class PostHandler(MyHandler):
-
-    def get(self): # /post
-        self.templateValues = {}
-        self.templateValues['title'] = 'Post'
-        logging.info('here we are')
-        self.render('base.html')            
-            
-
-        
-app = webapp2.WSGIApplication([('/blog.*', BlogHandler),('/post.*', PostHandler),
-                               ('/.*', MyHandler)], debug = True)
+app = webapp2.WSGIApplication([ ('/blog.*', BlogHandler),
+                                ('/.*', MainpageHandler)], debug = True)
     
 
 
