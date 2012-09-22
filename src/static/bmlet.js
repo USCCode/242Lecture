47e03248ffcd22bd3434ddc541fa89204e5471c2
Jose M Vidal <jmvidal@gmail.com>
2012-09-22 19:02:56 -0400

    Bookmarklet screencast

---
+++ src/static/bmlet.js
@@ -0,0 +1,7 @@
+console.log('loaded bmlet.js')
+
+
+links = document.getElementsByTagName('a');
+for (l in links){
+	links[l].innerText = 'blah';
+}
---
+++ src/static/script.js
@@ -0,0 +1,7 @@
+console.log('Loaded script.js');
+
+//function(){
+//	_s = document.createElement('script');
+//	_s.src = 'http://localhost:8080/static/bmlet.js';
+//	document.getElementsByTagName('head')[0].appendChild(_s);
+//}
