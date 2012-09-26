a4e2f401a193f7883e47457740140a24d4279dcc
Jose M Vidal <jmvidal@gmail.com>
2012-09-26 09:56:39 -0400

    Sep 26 end of lecture.

---
+++ src/static/script.js
@@ -0,0 +1,26 @@
+
+
+console.log('Hello');
+
+function double(x){
+	return 2*x
+}
+
+function f(g, x) {
+	return g(x);
+}
+
+a = {
+	name: 'bob',
+	cousin: {}
+};
+
+function print(a){
+	var i;
+	
+	for (i in a){
+		var b;
+		console.log(i);
+	}
+	b 
+}
