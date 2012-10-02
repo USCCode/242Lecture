89999f1114d781e1ad80b775c557b5c9ed2aa1b6
Jose M Vidal <jmvidal@gmail.com>
2012-10-02 13:52:51 -0400

    ajax video

--- src/main.py
+++ src/main.py
@@ -12,45 +12,23 @@ from google.appengine.ext import db
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
 
-class Post(db.Model):
-    content = db.StringProperty()
-    stars = db.IntegerProperty()
+class Comment(db.Model):
+    text = db.StringProperty()
 
-class MyHandler(webapp2.RequestHandler):
-    
-    def setupUser(self):
-        self.user = users.get_current_user()
-        if self.user:
-            self.templateValues['login'] = ('%s <a href="%s">Logout</a> ' 
-                                            % (self.user.nickname(), users.create_logout_url('/')))
-        else:
-            self.templateValues['login'] = ('<a href="%s">Login</a>' % 
-                                            users.create_login_url(self.request.uri))
-            
-    def render(self,afile):
-        template = jinja_environment.get_template(afile)
-        self.response.out.write(template.render(self.templateValues))
-        
-class MainpageHandler(MyHandler):
+class MainpageHandler(webapp2.RequestHandler):
     def get(self): # /
         self.templateValues = {}
-        self.templateValues['title'] = 'Main Page'
-        self.setupUser()
-        self.render("base.html")
+        self.templateValues['title'] = 'jQuery Ajax Tutorial'
+        template = jinja_environment.get_template("base.html")
+        self.response.out.write(template.render(self.templateValues))
         
-class BlogHandler(MyHandler):
-    def get(self):
-        self.templateValues = {}
-        self.templateValues['title'] = 'Blog Page'
-        self.setupUser()
-        if not self.user:
-            self.redirect('/')
-            return
-        self.render('base.html')
+    def post(self):
+        com = Comment(text=self.request.get('text'))
+        com.put()
+        self.response.out.write("Got it!")
+        
 
-        
-app = webapp2.WSGIApplication([ ('/blog.*', BlogHandler),
-                                ('/.*', MainpageHandler)], debug = True)
+app = webapp2.WSGIApplication([('/.*', MainpageHandler)], debug = True)
     
 
 
--- src/static/script.js
+++ src/static/script.js
@@ -1,1 +1,23 @@
 console.log('Loaded script.js');
+
+function handleResponse(data){
+	console.log('Got from server: ' + data);
+}
+
+function handleClick(e){
+	var text = $('#comment').val();
+	console.log('user typed ' + text);
+	$('#output').append('<li>' + text + '</li>');
+	$.ajax('/', {
+		type: 'POST',
+		data: {
+			text: text
+		},
+		success: handleResponse,
+		error: 
+	});
+}
+
+$(document).ready(function(){
+	$('#myButton').click(handleClick);
+})
