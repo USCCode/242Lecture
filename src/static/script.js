80d5519d330ff3c1d08889fd69dc86f475192f9b
Jose M Vidal <jmvidal@gmail.com>
2012-10-02 11:03:17 -0400

    Oct 1 lecture.

--- src/static/script.js
+++ src/static/script.js
@@ -1,14 +1,1 @@
 console.log('Loaded script.js');
-
-function userClick(e){ //event handler
-	console.log('clicked');
-	$('#message').toggle();
-}
-
-function hideThis(e){
-	console.log('x=' + e.pageX);
-}
-
-$(document).ready(function(){ //after  page is loaded
-	$('p').mousemove(hideThis); //set up event handler
-})
