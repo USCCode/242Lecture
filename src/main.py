a5997456621c8a925518cf4ca49baf913f9558ca
Jose M Vidal <jmvidal@gmail.com>
2012-10-11 14:41:08 -0400

    Json video.

--- src/main.py
+++ src/main.py
@@ -9,14 +9,19 @@ import os
 import logging #for debugging.
 from google.appengine.api import users
 from google.appengine.ext import db
+import json
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
 
-
 class MainpageHandler(webapp2.RequestHandler):
-    def get(self): # /
+    def get(self): # if / then return HTML, if /?fmt=json return json,  /item/123 html /item/123.json
+        if self.request.get('fmt') == 'json':
+            data = {'name': 'bob', 'age': 55}
+            self.response.out.headers['Content-Type'] = 'text/json'
+            self.response.out.write(json.dumps(data))
+            return
         self.templateValues = {}
-        self.templateValues['title'] = 'jQuery Events Tutorial'
+        self.templateValues['title'] = 'jQuery JSON Tutorial'
         template = jinja_environment.get_template("base.html")
         self.response.out.write(template.render(self.templateValues))
         
--- src/static/script.js
+++ src/static/script.js
@@ -1,6 +1,24 @@
 console.log('Loaded script.js');
 
-$(document).ready(function(){
+var theData; //the Model :: Model-View Controller 
 
-	
+function showData(data){
+	console.log(data.name);
+	theData = data;
+	$('#result').html(data.name)
+}
+
+function handleClick(e){
+	$.ajax('/1',{
+		type: 'GET',
+		data: {
+			fmt: 'json'
+		},
+		success: showData
+	});
+}
+
+
+$(document).ready(function(){
+	handleClick();
 });
