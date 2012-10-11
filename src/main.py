e0f64de50cea99ec36e949bfcdb05396e4e5261f
Jose M Vidal <jmvidal@gmail.com>
2012-10-11 13:52:55 -0400

    Cleanup for video.

--- src/main.py
+++ src/main.py
@@ -12,39 +12,14 @@ from google.appengine.ext import db
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
 
-class Comment(db.Model):
-    text = db.StringProperty()
 
 class MainpageHandler(webapp2.RequestHandler):
     def get(self): # /
         self.templateValues = {}
         self.templateValues['title'] = 'jQuery Events Tutorial'
-        self.templateValues['comments'] = Comment.all()
         template = jinja_environment.get_template("base.html")
         self.response.out.write(template.render(self.templateValues))
         
-    def post(self):
-        logging.info('Got a POST')
-#        self.abort(500)
-        text = self.request.get('text')
-        logging.info('Got a POST text=%s=' % text)
-        c = Comment(text=text)
-        c.put()
-        self.response.out.write("DONE")
-        
-    def delete(self):
-#        self.abort(500)
-        text = self.request.get('text') # SO, it seems that the DELETE does not accept any arguments.
-                                        # I don't know if that is a webapp2 thing, or that is part of 
-                                        # the HTTP specification. 
-        logging.info('Got a DELETE text=%s=' % text)
-        query = Comment.all().filter('text =', text)
-        for c in query:
-            logging.info("Deleting %s" % c.text)
-            c.delete()
-        self.response.out.write("DONE")
-        
-
 app = webapp2.WSGIApplication([('/.*', MainpageHandler)], debug = True)
     
 
--- src/static/script.js
+++ src/static/script.js
@@ -1,44 +1,6 @@
 console.log('Loaded script.js');
 
-function handleButtonClick(e){
-	console.log('User clicked');
-	var text = $('#comment').val();
-	var newElement = document.createElement('li');
-	newElement.innerHTML = text;
-	$('#comments').append(newElement);
-	///put val() into server????
-	$.ajax('/', {
-		type: 'POST',
-		data: {
-			text: text
-		},
-		success: function(){			
-			console.log('Success!!');
-		},
-		error: function() {
-			console.log('Error at server:');
-			newElement.remove();
-		}
-	});
-	e.stopPropagation(); //comment this out so see..
-}
-
-function handleDelete(e){
-	console.log('Delete');
-	console.log($(this));
-	var text = $(this).html();
-	$(this).remove();
-	$.ajax('/', {
-		type: 'DELETE',
-		data: { 
-			text: text
-		}
-	});
-}
-	
 $(document).ready(function(){
-	$('#myButton').on("click", handleButtonClick); 
 
-	$('#menu').on("click", 'li', handleDelete);
 	
 });
